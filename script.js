4c74d2ca098f01bc31db385efb126220b28c7b7c
KJ <147823964+Kayjayness@users.noreply.github.com>
2024-05-01 22:20:27 +0200

    Added ability to reset sketchpad as well as hover coloring for the tiles

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ let gridAmount = null;
 let gridLength = null;
 
 generateButton.addEventListener ("click", () => {
+    removeElements();
     gridAmount = prompt("How many squares would you like your drawing pad to contain?");
     gridLength = 100 / gridAmount;
-    console.log(gridLength);
     createSketchArea();
 });
+
+function removeElements() {
+    while(areaContainer.firstChild) {
+        areaContainer.removeChild(areaContainer.firstChild);
+        console.log("removed")
+    }
+}
 
 function createSketchArea() {
     for(let i = 1; i <= gridAmount; ++i) {
         for(let j = 1; j <= gridAmount; ++j) {
             let createDivs = document.createElement("div");
             createDivs.classList.add("areaDivs");
-            areaContainer.appendChild(createDivs);
+            areaContainer.appendChild(createDivs)
             createDivs.style.flex = `1 1 ${gridLength}%`;
-            
+
+            createDivs.addEventListener("mouseover", () => {
+                createDivs.style.backgroundColor = "#555";
+            });
         }
     }
 }
